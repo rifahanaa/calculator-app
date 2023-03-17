8a839b615e8a3fb71b3d744a03e4f58d75b27e21
rifahana <rifahn022@gmail.com>
2023-03-17 14:49:28 +0700

    menambahkan file calculator.cs

---
+++ Calculator.cs
@@ -0,0 +1,17 @@
+class Calculator
+{
+	static void Main(string[] args)
+	{
+		int a = 10;
+	
+	}
+	static int Penambahan(int a, int b)
+	{
+		return a+b;
+	}
+	
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
+	}
+}
